3b09672564695d0d26c613cd6db71ebf847476ec
Nam9396 <nguyenthanhnam9396@gmail.com>
2025-01-20 08:54:51 +0700

    set back scroll to true

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -40,7 +40,6 @@ function NavLink({
     <Link
       href={href}
       aria-current={active ? 'page' : undefined}
-      scroll={false}
       className={clsx(
         'flex justify-between gap-2 py-1 pr-3 text-sm transition',
         isAnchorLink ? 'pl-7' : 'pl-4',
